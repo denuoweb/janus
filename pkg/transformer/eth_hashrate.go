fac34b7f5354bb19d2c2529b1f74ce41ecd83b45
Jaron Rosenau <jaron.rosenau@gmail.com>
2022-02-04 08:52:44 -0800

    port to htmlcoin

--- pkg/transformer/eth_hashrate.go
+++ pkg/transformer/eth_hashrate.go
@@ -5,13 +5,13 @@ import (
 
 	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/labstack/echo"
-	"github.com/qtumproject/janus/pkg/eth"
-	"github.com/qtumproject/janus/pkg/qtum"
+	"github.com/htmlcoin/janus/pkg/eth"
+	"github.com/htmlcoin/janus/pkg/htmlcoin"
 )
 
 //ProxyETHGetHashrate implements ETHProxy
 type ProxyETHHashrate struct {
-	*qtum.Qtum
+	*htmlcoin.Htmlcoin
 }
 
 func (p *ProxyETHHashrate) Method() string {
@@ -23,17 +23,17 @@ func (p *ProxyETHHashrate) Request(_ *eth.JSONRPCRequest, c echo.Context) (inter
 }
 
 func (p *ProxyETHHashrate) request() (*eth.HashrateResponse, eth.JSONRPCError) {
-	qtumresp, err := p.Qtum.GetHashrate()
+	htmlcoinresp, err := p.Htmlcoin.GetHashrate()
 	if err != nil {
 		return nil, eth.NewCallbackError(err.Error())
 	}
 
-	// qtum res -> eth res
-	return p.ToResponse(qtumresp), nil
+	// htmlcoin res -> eth res
+	return p.ToResponse(htmlcoinresp), nil
 }
 
-func (p *ProxyETHHashrate) ToResponse(qtumresp *qtum.GetHashrateResponse) *eth.HashrateResponse {
-	hexVal := hexutil.EncodeUint64(math.Float64bits(qtumresp.Difficulty))
+func (p *ProxyETHHashrate) ToResponse(htmlcoinresp *htmlcoin.GetHashrateResponse) *eth.HashrateResponse {
+	hexVal := hexutil.EncodeUint64(math.Float64bits(htmlcoinresp.Difficulty))
 	ethresp := eth.HashrateResponse(hexVal)
 	return &ethresp
 }
