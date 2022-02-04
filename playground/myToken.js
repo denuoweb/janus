fb0b24a844ec3a05193b0997b38ba24053b91ab6
Jaron Rosenau <jaron.rosenau@gmail.com>
2022-02-04 03:05:04 -0800

    port to htmlcoin

--- playground/myToken.js
+++ playground/myToken.js
@@ -1,20 +1,20 @@
 const url = require('url');
 
 const rpcURL =  process.env.ETH_RPC;
-const qtumAccount  = url.parse(rpcURL).auth.split(":")[0]
+const htmlcoinAccount  = url.parse(rpcURL).auth.split(":")[0]
 
 // assume: node 8 or above
 const ora = require("ora")
 const parseArgs = require("minimist")
 
-const qtum = require("qtumjs")
-const rpc = new qtum.EthRPC(rpcURL, qtumAccount)
+const htmlcoin = require("htmlcoinjs")
+const rpc = new htmlcoin.EthRPC(rpcURL, htmlcoinAccount)
 const repoData = require("./solar.development.json")
 const {
   sender,
   ...info
 } = repoData.contracts['contracts/MyToken.sol']
-const myToken = new qtum.Contract(rpc, info)
+const myToken = new htmlcoin.Contract(rpc, info)
 
 const opts = {gasPrice: 100}
 
