f82ece5913a5341a7ab2e20eb998ba367ed20915
Jaron Rosenau <jaron.rosenau@gmail.com>
2022-02-04 07:52:09 -0800

    port to htmlcoin

--- pkg/transformer/log.go
+++ pkg/transformer/log.go
@@ -3,25 +3,25 @@ package transformer
 import (
 	"github.com/go-kit/kit/log"
 	"github.com/go-kit/kit/log/level"
-	"github.com/qtumproject/janus/pkg/qtum"
+	"github.com/htmlcoin/janus/pkg/htmlcoin"
 )
 
-func GetLogger(proxy ETHProxy, q *qtum.Qtum) log.Logger {
+func GetLogger(proxy ETHProxy, q *htmlcoin.Htmlcoin) log.Logger {
 	method := proxy.Method()
 	logger := q.Client.GetLogger()
 	return log.WithPrefix(level.Info(logger), method)
 }
 
 func GetLoggerFromETHCall(proxy *ProxyETHCall) log.Logger {
-	return GetLogger(proxy, proxy.Qtum)
+	return GetLogger(proxy, proxy.Htmlcoin)
 }
 
-func GetDebugLogger(proxy ETHProxy, q *qtum.Qtum) log.Logger {
+func GetDebugLogger(proxy ETHProxy, q *htmlcoin.Htmlcoin) log.Logger {
 	method := proxy.Method()
 	logger := q.Client.GetDebugLogger()
 	return log.WithPrefix(level.Debug(logger), method)
 }
 
 func GetDebugLoggerFromETHCall(proxy *ProxyETHCall) log.Logger {
-	return GetDebugLogger(proxy, proxy.Qtum)
+	return GetDebugLogger(proxy, proxy.Htmlcoin)
 }
