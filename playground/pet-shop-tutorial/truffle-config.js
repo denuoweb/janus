2db7844bb158953d862295bcf0df2f4d5564e2e1
Jaron Rosenau <jaron.rosenau@gmail.com>
2022-02-04 05:31:09 -0800

    port to htmlcoin

--- playground/pet-shop-tutorial/truffle-config.js
+++ playground/pet-shop-tutorial/truffle-config.js
@@ -4,7 +4,7 @@ module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
-      port: 23889,
+      port: 24889,
       network_id: "*",
       from: "0x7926223070547d2d15b2ef5e7383e541c338ffe9",
       gasPrice: "0x64"
@@ -16,7 +16,7 @@ module.exports = {
     },
     testnet: {
       host: "hk1.s.qtum.org",
-      port: 23889,
+      port: 24889,
       network_id: "*",
       from: "0x7926223070547d2d15b2ef5e7383e541c338ffe9",
       gasPrice: "0x64"
