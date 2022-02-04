65bcad38176b2c372e1f7e2f6c0023e232ab8c86
Jaron Rosenau <jaron.rosenau@gmail.com>
2022-02-04 08:44:46 -0800

    port to htmlcoin

--- pkg/transformer/eth_newBlockFilter.go
+++ pkg/transformer/eth_newBlockFilter.go
@@ -3,13 +3,13 @@ package transformer
 import (
 	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/labstack/echo"
-	"github.com/qtumproject/janus/pkg/eth"
-	"github.com/qtumproject/janus/pkg/qtum"
+	"github.com/htmlcoin/janus/pkg/eth"
+	"github.com/htmlcoin/janus/pkg/htmlcoin"
 )
 
 // ProxyETHNewBlockFilter implements ETHProxy
 type ProxyETHNewBlockFilter struct {
-	*qtum.Qtum
+	*htmlcoin.Htmlcoin
 	filter *eth.FilterSimulator
 }
 
