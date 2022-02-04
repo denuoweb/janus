70d0f23739526f93631935d3e31768f4f491746f
Jaron Rosenau <jaron.rosenau@gmail.com>
2022-02-04 06:00:03 -0800

    port to htmlcoin

--- playground/docker/init-container.sh
+++ playground/docker/init-container.sh
@@ -1,32 +1,32 @@
 #!/bin/sh
 while
-     qcli help &> /dev/null
+     hcli help &> /dev/null
      rc=$?; if [[ $rc == 0 ]]; then break; fi
 do :;  done
 
-balance=`qcli getbalance`
+balance=`hcli getbalance`
 if [ "${balance:0:1}" == "0" ]
 then
     set -x
-	qcli generate 600 > /dev/null
+	hcli generate 600 > /dev/null
 	set -
 fi
 
 WALLETFILE=test-wallet
-LOCKFILE=${QTUM_DATADIR}/import-test-wallet.lock
+LOCKFILE=${HTMLCOIN_DATADIR}/import-test-wallet.lock
 
 if [ ! -e $LOCKFILE ]; then
   while
-       qcli getaddressesbyaccount "" &> /dev/null
+       hcli getaddressesbyaccount "" &> /dev/null
        rc=$?; if [[ $rc != 0 ]]; then continue; fi
 
        set -x
 
-       qcli importprivkey "cMbgxCJrTYUqgcmiC1berh5DFrtY1KeU4PXZ6NZxgenniF1mXCRk" # addr=qUbxboqjBRp96j3La8D1RYkyqx5uQbJPoW hdkeypath=m/88'/0'/1'
-       qcli importprivkey "cRcG1jizfBzHxfwu68aMjhy78CpnzD9gJYZ5ggDbzfYD3EQfGUDZ" # addr=qLn9vqbr2Gx3TsVR9QyTVB5mrMoh4x43Uf hdkeypath=m/88'/0'/2'
+       hcli importprivkey "cMbgxCJrTYUqgcmiC1berh5DFrtY1KeU4PXZ6NZxgenniF1mXCRk" # addr=hUbxboqjBRp96j3La8D1RYkyqx5uQbJPoW hdkeypath=m/88'/0'/1'
+       hcli importprivkey "cRcG1jizfBzHxfwu68aMjhy78CpnzD9gJYZ5ggDbzfYD3EQfGUDZ" # addr=hLn9vqbr2Gx3TsVR9QyTVB5mrMoh4x43Uf hdkeypath=m/88'/0'/2'
 
-       solar prefund qUbxboqjBRp96j3La8D1RYkyqx5uQbJPoW 500
-       solar prefund qLn9vqbr2Gx3TsVR9QyTVB5mrMoh4x43Uf 500
+       solar prefund hUbxboqjBRp96j3La8D1RYkyqx5uQbJPoW 500
+       solar prefund hLn9vqbr2Gx3TsVR9QyTVB5mrMoh4x43Uf 500
        touch $LOCKFILE
 
        set -
