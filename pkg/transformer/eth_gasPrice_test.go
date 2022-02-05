29be0342c6aae85b41aa7e712e099a141ef725c1
Jaron Rosenau <jaron.rosenau@gmail.com>
2022-02-04 23:17:00 -0800

    port to htmlcoin

--- pkg/transformer/eth_gasPrice_test.go
+++ pkg/transformer/eth_gasPrice_test.go
@@ -5,7 +5,7 @@ import (
 	"reflect"
 	"testing"
 
-	"github.com/qtumproject/janus/pkg/internal"
+	"github.com/htmlcoin/janus/pkg/internal"
 )
 
 func TestGasPriceRequest(t *testing.T) {
@@ -17,13 +17,13 @@ func TestGasPriceRequest(t *testing.T) {
 	}
 
 	mockedClientDoer := internal.NewDoerMappedMock()
-	qtumClient, err := internal.CreateMockedClient(mockedClientDoer)
+	htmlcoinClient, err := internal.CreateMockedClient(mockedClientDoer)
 	if err != nil {
 		t.Fatal(err)
 	}
 
 	//preparing proxy & executing request
-	proxyEth := ProxyETHGasPrice{qtumClient}
+	proxyEth := ProxyETHGasPrice{htmlcoinClient}
 	got, jsonErr := proxyEth.Request(request, nil)
 	if jsonErr != nil {
 		t.Fatal(jsonErr)
