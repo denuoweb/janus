cfb39899cd555883baec894b6f31b31cecedc8e2
Jaron Rosenau <jaron.rosenau@gmail.com>
2022-02-05 01:33:46 -0800

    port to htmlcoin

--- pkg/transformer/eth_chainId.go
+++ pkg/transformer/eth_chainId.go
@@ -6,12 +6,12 @@ import (
 
 	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/labstack/echo"
-	"github.com/qtumproject/janus/pkg/eth"
-	"github.com/qtumproject/janus/pkg/qtum"
+	"github.com/htmlcoin/janus/pkg/eth"
+	"github.com/htmlcoin/janus/pkg/htmlcoin"
 )
 
 type ProxyETHChainId struct {
-	*qtum.Qtum
+	*htmlcoin.Htmlcoin
 }
 
 func (p *ProxyETHChainId) Method() string {
@@ -19,30 +19,30 @@ func (p *ProxyETHChainId) Method() string {
 }
 
 func (p *ProxyETHChainId) Request(req *eth.JSONRPCRequest, c echo.Context) (interface{}, eth.JSONRPCError) {
-	chainId, err := getChainId(p.Qtum)
+	chainId, err := getChainId(p.Htmlcoin)
 	if err != nil {
 		return nil, err
 	}
 	return eth.ChainIdResponse(hexutil.EncodeBig(chainId)), nil
 }
 
-func getChainId(p *qtum.Qtum) (*big.Int, eth.JSONRPCError) {
-	var qtumresp *qtum.GetBlockChainInfoResponse
-	if err := p.Request(qtum.MethodGetBlockChainInfo, nil, &qtumresp); err != nil {
+func getChainId(p *htmlcoin.Htmlcoin) (*big.Int, eth.JSONRPCError) {
+	var htmlcoinresp *htmlcoin.GetBlockChainInfoResponse
+	if err := p.Request(htmlcoin.MethodGetBlockChainInfo, nil, &htmlcoinresp); err != nil {
 		return nil, eth.NewCallbackError(err.Error())
 	}
 
 	var chainId *big.Int
-	switch strings.ToLower(qtumresp.Chain) {
+	switch strings.ToLower(htmlcoinresp.Chain) {
 	case "main":
-		chainId = big.NewInt(8888)
+		chainId = big.NewInt(4444)
 	case "test":
-		chainId = big.NewInt(8889)
+		chainId = big.NewInt(4445)
 	case "regtest":
-		chainId = big.NewInt(8890)
+		chainId = big.NewInt(4446)
 	default:
-		chainId = big.NewInt(8890)
-		p.GetDebugLogger().Log("msg", "Unknown chain "+qtumresp.Chain)
+		chainId = big.NewInt(4446)
+		p.GetDebugLogger().Log("msg", "Unknown chain "+htmlcoinresp.Chain)
 	}
 
 	return chainId, nil
